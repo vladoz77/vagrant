1f64bafcdb9a74c7c50427ee5d1162df0444e30d
Kovalev Vladislav <vladoz77@yandex.ru>
2023-12-07 11:34:42 +0300

    fix add-user.sh

--- ubuntu/workfolder/add-user.sh
+++ ubuntu/workfolder/add-user.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/bash
+# Check root user
+if [ ${UID} -ne 0 ]
+then 
+  echo Login by root user
+  exit 1
+fi
+
 
 # Ask username
 read -p 'Enter username: ' USER_NAME
+id -u $USER_NAME  > /dev/null 2>&1
+if [ ${?} -eq 0 ]
+then 
+  echo 'user exist, try new name'
+  exit 1
+fi
 
 # Ask real name
 read -p 'Enter real name : ' USER
@@ -11,6 +24,11 @@ read -p 'Enter password: ' PASS
 
 # Add user
 useradd -c "${USER}" -m "${USER_NAME}"
+if [ ${?} -ne 0 ]
+then 
+  echo 'User did not create'
+  exit 1
+fi
 
 # Add password
 echo "${USER_NAME}":"${PASS}" | chpasswd
@@ -19,4 +37,5 @@ echo "${USER_NAME}":"${PASS}" | chpasswd
 passwd -e "${USER_NAME}"
 
 echo User "${USER_NAME}" created
+exit 0
  
