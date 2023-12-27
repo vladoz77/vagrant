b557e2e093a8184a144d12342fd88df478c3fe41
Kovalev Vladislav <vladoz77@yandex.ru>
2023-12-27 16:01:58 +0300

    fix

--- ubuntu/workfolder/add-new-user.sh
+++ ubuntu/workfolder/add-new-user.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Make sure the script start from root
 if [ ${UID} -ne 0 ]; then
 echo "Please start script from root"
@@ -7,32 +6,35 @@ exit 1
 fi
 
 # Check if has arguments
-if [ ${#} -eq 0 ]; then
+if [ ${#} -lt 1 ]; then
 echo "Please enter username"
 exit 1
 fi
 
-
-# First param is username
+# First param is username, comment, password
 USERNAME=${1}
-COMMENT=${2}
+
+# Add comment line
+shift
+COMMENT=${@}
+
+# Generate password
 PASSWORD="$(date +%N%s| sha256sum | head -c8)"
 
 # Check if user exist
-id ${USERNAME} > /dev/null 2>&1
-if [ ${?} -eq 0 ]; then
+if id ${USERNAME} > /dev/null 2>&1; then
 echo "User ${USERNAME} is exist"
 exit 1
 fi
 
 # Create user
-useradd -m -c ${COMMENT} -s /bin/bash  ${USERNAME}
+useradd -m -c "${COMMENT}" -s /bin/bash "${USERNAME}"
 echo "${USERNAME}":"${PASSWORD}" | chpasswd
 
 # Change pass by first login
-passwd -e "${USERNAME}" > /dev/null 2>&1
+passwd -e "${USERNAME}" > /dev/null
 
 # user info
-echo "username is: ${USERNAME}"
-echo "password is: ${PASSWORD}"
+echo "username: ${USERNAME}"
+echo "password: ${PASSWORD}"
 
