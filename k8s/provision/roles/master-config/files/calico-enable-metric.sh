89d43e2fa01347d700e4195e9d2828f10e9fcabb
Kovalev Vladislav <vladoz77@yandex.ru>
2025-07-04 17:02:16 +0300

    add new k8s role

---
+++ k8s/provision/roles/master-config/files/calico-enable-metric.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "wait while namespace calico-system create"
+sleep 30
+# Enable metrics 
+kubectl patch felixconfiguration default --type merge --patch '{"spec":{"prometheusMetricsEnabled": true}}' 
+kubectl patch installation default --type=merge -p '{"spec": {"typhaMetricsPort":9093}}'
+
+# Create service for calico node
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: calico-felix-metrics
+  namespace: calico-system
+spec:
+  selector:
+    k8s-app: calico-node
+  ports:
+  - port: 9091
+    targetPort: 9091
+EOF
